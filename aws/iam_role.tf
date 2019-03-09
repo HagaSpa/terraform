a4916c5e000f13a317470c7a0ffcd76f0f4a159d
HagaSpa <justicesparrow13@gmail.com>
2019-03-09 16:36:47 +0900

    iam_policyをaws側で提供されている同等のものに変更。lambdaと適切なiam_roleを追加

--- aws/iam_role.tf
+++ aws/iam_role.tf
@@ -17,3 +17,23 @@ resource "aws_iam_role" "iam_for_lambda_s3_full_access" {
 }
 EOF
 }
+
+resource "aws_iam_role" "iam_for_lambda_execution" {
+  name = "iam_for_lambda_execution"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
--- aws/lambda.tf
+++ aws/lambda.tf
@@ -1,13 +1,22 @@
-resource "aws_lambda_function" "s3_full_access_lambda" {
+resource "aws_lambda_function" "s3_check_lambda" {
   filename      = "src/lambda_function_payload.zip"
-  function_name = "s3_full_access_lambda"
+  function_name = "s3_check_lambda"
   role          = "${aws_iam_role.iam_for_lambda_s3_full_access.arn}"
   handler       = "lambda_function.lambda_function"
   runtime       = "python3.7"
 
   environment {
     variables = {
-      foo = "bar"
+      bucket = "hagaspa-bucket-1"
     }
   }
+}
+
+resource "aws_lambda_function" "post_result_lambda" {
+  filename      = "src/lambda_function_payload.zip"
+  function_name = "post_result_lambda"
+  role          = "${aws_iam_role.iam_for_lambda_execution.arn}"
+  handler       = "lambda_function.lambda_function"
+  runtime       = "python3.7"
+
 }
